2c00b4220e072e48dfd5bc37721a9c613fc186ec
javier <jv2300220@gamil.com>
2025-07-22 07:25:33 -0600

    estructura del sistema de asistenciasç

---
+++ funciones JavaScript/funciones.js
@@ -0,0 +1,1 @@
+console.log("Sistema INSAM cargado correctamente.");
---
+++ php/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Sistema de Asistencias INSAM</title>
+  <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+  <h1>Bienvenido al Sistema de Asistencias INSAM</h1>
+  <script src="js/funciones.js"></script>
+</body>
+</html>
